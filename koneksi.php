0471d623980253f19c594e8940bb95a08c907a7f
Muhammad Dio Syahputra <diosyahputra98765@gmail.com>
2021-06-07 13:23:28 +0800

    melakukan penambahan file koneksi.php, sukses menghubungkan tabel dengan database phpmyadmin

---
+++ koneksi.php
@@ -0,0 +1,7 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'db_pln');
+
+$mysqli = mysqli_connect(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
